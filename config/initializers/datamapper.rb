7501b3e7044f465f0361ac64e6e64586b47bbb87
François Beausoleil <francois@teksol.info>
2008-10-17 08:58:15 -0400

    Fixed database configuration loading.
    Was leaking a file.

--- config/initializers/datamapper.rb
+++ config/initializers/datamapper.rb
@@ -1,3 +1,3 @@
 require "dm-core"
-hash = YAML.load(File.new(Rails.root + "/config/database.yml"))
+hash = YAML.load_file(File.join(Rails.root, "config", "database.yml"))
 DataMapper.setup(:default, hash[Rails.env])
